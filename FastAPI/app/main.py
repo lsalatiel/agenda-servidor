deb5b3e449c43ef7a9939c0c61e098eedd426da5
naascimento0 <gabrieloliveiran@hotmail.com>
2024-06-20 11:18:29 -0300

    adding user router

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routers import user
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+app.include_router(user.router)
