1e689c0cb627f95ef8d19c20f9fa9f6703691734
lsalatiel <lucassalatiel1004@gmail.com>
2024-06-20 22:38:11 -0300

    prettier update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react"
+import { ChakraProvider } from "@chakra-ui/react"
+import App from "./App.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// 1. Import the extendTheme function
+import * as ReactDOM from "react-dom/client"
+
+const rootElement = document.getElementById("root")
+ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </React.StrictMode>
 )
