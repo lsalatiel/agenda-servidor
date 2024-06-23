5ddf0f7efefa5b7ea9e43f8b5c57530e4a633c4b
lsalatiel <lucassalatiel1004@gmail.com>
2024-06-22 21:45:16 -0300

    update main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
-import Login from "./components/Login"
+import Login from "./views/Login"
+import Home from "./views/Home"
+// import Navbar from "./components/Navbar"
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <Login />
+            <Home />
         </>
     )
 }
