f313dd99794311b49ad05ede1ce191a189805646
naascimento0 <gabrieloliveiran@hotmail.com>
2024-06-29 08:16:49 -0300

    update methods to async

--- FastAPI/app/routers/area.py
+++ FastAPI/app/routers/area.py
@@ -6,7 +6,7 @@ from app import schemas, models
 router = APIRouter(prefix="/areas", tags=['Areas'])
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.AreaResponse)
-def create_area(area: schemas.AreaCreate, db: Session = Depends(get_db)):
+async def create_area(area: schemas.AreaCreate, db: Session = Depends(get_db)):
     area_id = db.query(models.Area).filter(models.Area.id == area.id).first()
     if area_id:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Area with ID {area.id} already exists")
@@ -17,14 +17,14 @@ def create_area(area: schemas.AreaCreate, db: Session = Depends(get_db)):
     return new_area
 
 @router.get("/{id}", response_model=schemas.AreaResponse)
-def get_area(id: str, db: Session = Depends(get_db)):
+async def get_area(id: str, db: Session = Depends(get_db)):
     area = db.query(models.Area).filter(models.Area.id == id).first()
     if not area:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Area with ID {id} not found")
     return area
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_area(id: str, db: Session = Depends(get_db)):
+async def delete_area(id: str, db: Session = Depends(get_db)):
     area = db.query(models.Area).filter(models.Area.id == id).first()
     if not area:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Area with ID {id} not found")
