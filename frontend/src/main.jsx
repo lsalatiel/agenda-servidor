efa198cec8e78deffef848a8401a64ee9a117be1
lsalatiel <lucassalatiel1004@gmail.com>
2024-07-01 17:42:02 -0300

    add routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
 import Login from "./views/Login.jsx";
 import Register from "./views/Register.jsx";
+import Schedule from "./views/Schedule.jsx";
+import ScheduleList from "./views/ScheduleList.jsx";
 
 // 1. Import the extendTheme function
 import * as ReactDOM from "react-dom/client";
@@ -20,6 +22,14 @@ const router = createBrowserRouter([
     {
         path: "/register",
         element: <Register />,
+    },
+    {
+        path: "/agendar",
+        element: <Schedule />,
+    },
+    {
+        path: "/meus-agendamentos",
+        element: <ScheduleList />,
     },
 ]);
 
