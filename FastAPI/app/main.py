d655047debea2efaa4be2634faaa6e819062adc3
naascimento0 <gabrieloliveiran@hotmail.com>
2024-07-04 11:12:22 -0300

    new 3000 port

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -8,7 +8,8 @@ origins = [
     'http://localhost:8000',
     'http://0.0.0.0:8000',
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'http://localhost:3000'
 ]
 
 app.add_middleware(
